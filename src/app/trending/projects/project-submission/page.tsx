06d64c08ff54e86e69e9ca16e2b0a26b5b738450
sean00880 <shantanu1997@gmail.com>
2024-11-14 09:06:35 -0800

    Initial commit2

--- src/app/trending/projects/project-submission/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectFormSubmission from '../../../../components/ProjectFormSubmission';
-
-export default function ProjectSubmitPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-[#090909] text-white">
-      <ProjectFormSubmission />
-    </div>
-  );
-}
