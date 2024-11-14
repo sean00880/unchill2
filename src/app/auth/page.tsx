bb066ba9237ed2a9f102eee867d816239f9ca31d
sean00880 <shantanu1997@gmail.com>
2024-11-14 10:10:35 -0800

    Initial commit4

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,10 +1,10 @@
 // src/app/auth/page.tsx
 import AuthForm from '../../components/AuthForm';
 
-export default function AuthPage({ isDarkMode }: { isDarkMode: boolean }) {
+export default function AuthPage() {
   return (
-    <div className={`dashboard main-content min-h-screen flex items-center justify-center ${isDarkMode ? 'bg-dark' : 'bg-light'} text-white`}>
-      <AuthForm isDarkMode={isDarkMode} />
+    <div className="dashboard main-content min-h-screen flex items-center justify-center bg-dark text-white">
+      <AuthForm isDarkMode={true} /> {/* Pass a boolean or modify this as needed */}
     </div>
   );
 }
