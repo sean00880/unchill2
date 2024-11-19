e2b1f40769fd23550c222c4413273609b0f4627a
sean00880 <shantanu1997@gmail.com>
2024-11-18 20:53:36 -0800

    Edit36

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,4 +1,3 @@
-// src/components/AuthForm.tsx
 "use client";
 import { useState, useCallback } from "react";
 import Image from "next/image";
@@ -14,75 +13,55 @@ export default function AuthForm({ isDarkMode }: { isDarkMode: boolean }) {
   const [username, setUsername] = useState("");
   const [about, setAbout] = useState("");
   const [profilePicture, setProfilePicture] = useState<string | null>(null);
+  const [bannerImage, setBannerImage] = useState<string | null>(null);
   const totalSteps = 3;
 
   const nextStep = () => setCurrentStep((prev) => Math.min(prev + 1, totalSteps));
   const prevStep = () => setCurrentStep((prev) => Math.max(prev - 1, 1));
 
   const containerStyle = `
-    ${isDarkMode ? "bg-[#090909]/80" : "bg-white/80"} 
+   
     backdrop-blur-md border rounded-lg p-6 shadow-lg
   `;
 
-  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleImageUpload = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    setImage: React.Dispatch<React.SetStateAction<string | null>>
+  ) => {
     const file = event.target.files?.[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
-        setProfilePicture(reader.result as string);
+        setImage(reader.result as string);
       };
       reader.readAsDataURL(file);
     }
   };
 
   const handleChange = useCallback(
-    (setter: React.Dispatch<React.SetStateAction<string>>) => (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    (setter: React.Dispatch<React.SetStateAction<string>>) => (
+      e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
       setter(e.target.value);
     },
     []
   );
 
-  const renderMembershipComparison = () => (
-    <div className="mt-8">
-      <h3 className="font-bold text-lg mb-4">Membership Comparison</h3>
-      <table className="w-full text-left border-collapse border border-gray-500">
-        <thead>
-          <tr>
-            <th className="border border-gray-500 p-2">Feature</th>
-            <th className="border border-gray-500 p-2">Basic</th>
-            <th className="border border-gray-500 p-2">Verified</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="border border-gray-500 p-2">Access to platform</td>
-            <td className="border border-gray-500 p-2">Limited</td>
-            <td className="border border-gray-500 p-2">Full</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 p-2">Profile visibility</td>
-            <td className="border border-gray-500 p-2">Standard</td>
-            <td className="border border-gray-500 p-2">High</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 p-2">Support level</td>
-            <td className="border border-gray-500 p-2">Basic</td>
-            <td className="border border-gray-500 p-2">Priority</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+  const customUploadButtonStyle = `
+    py-2 px-4 mt-2 bg-white/20 border border-white/30 rounded-lg
+    shadow-xl backdrop-blur-md hover:bg-white/30 transition-all duration-300
+    cursor-pointer transform hover:scale-105 text
+  `;
 
   return (
-    <div className="flex flex-col w-full h-screen relative">
+    <div className="flex flex-col w-full min-h-screen">
       {/* Fixed Progress Bar at the Top */}
       <div className="w-full sticky top-0 z-10 bg-transparent">
         <ProgressBar currentStep={currentStep} totalSteps={totalSteps} />
       </div>
 
       {/* Main Content Container */}
-      <div className="flex flex-1 flex-col md:flex-row justify-center items-center md:items-stretch">
+      <div className="flex flex-1 flex-col md:flex-row justify-center items-stretch">
         {/* Left Column: Form */}
         <div className="flex-1 w-full md:w-1/2 p-4 md:p-6 flex justify-center items-center">
           <div className={`${containerStyle} w-full`}>
@@ -113,7 +92,7 @@ export default function AuthForm({ isDarkMode }: { isDarkMode: boolean }) {
               <form>
                 <span>Create Your Profile:</span>
                 <div className="mt-2 space-y-4">
-                  <label className="block">Display Name</label>
+                  <label className="block text">Display Name</label>
                   <input
                     type="text"
                     placeholder="Enter your display name..."
@@ -121,7 +100,7 @@ export default function AuthForm({ isDarkMode }: { isDarkMode: boolean }) {
                     value={displayName}
                     onChange={handleChange(setDisplayName)}
                   />
-                  <label className="block mt-4">Username</label>
+                  <label className="block text mt-4">Username</label>
                   <input
                     type="text"
                     placeholder="Enter your username..."
@@ -129,20 +108,13 @@ export default function AuthForm({ isDarkMode }: { isDarkMode: boolean }) {
                     value={username}
                     onChange={handleChange(setUsername)}
                   />
-                  <label className="block mt-4">About/Introduction</label>
+                  <label className="block text mt-4">About/Introduction</label>
                   <textarea
                     placeholder="Tell us about yourself or your role..."
                     className="w-full p-2 border rounded-md bg-transparent"
                     value={about}
                     onChange={handleChange(setAbout)}
                   />
-                  {/* Upload Profile Picture for larger screens */}
-                  {currentStep === 2 && (
-                    <div className="block md:hidden mt-4">
-                      <label className="block">Upload Profile Picture</label>
-                      <input type="file" className="w-full" onChange={handleImageUpload} />
-                    </div>
-                  )}
                 </div>
               </form>
             )}
@@ -150,18 +122,33 @@ export default function AuthForm({ isDarkMode }: { isDarkMode: boolean }) {
             {currentStep === 3 && (
               <div className="space-y-4">
                 <h3 className="text-xl font-bold">Profile Setup</h3>
-                <p className="text-sm">Customize your profile based on the selected membership tier.</p>
-                <label className="block">Membership Tier</label>
-                <select
-                  className="w-full p-2 border rounded-md bg-transparent"
-                  value={membershipTier}
-                  onChange={(e) => setMembershipTier(e.target.value)}
-                >
-                  <option value="basic">Basic (Free for a limited time)</option>
-                  <option value="verified">Verified (Paid) | Recommended for Maximum Exposure</option>
-                </select>
-                
-                <label className="block mt-4">Profile</label>
+                <p className="text-sm">Choose your membership tier:</p>
+                <div className="flex items-center space-x-4">
+                  <label className="flex text items-center">
+                    <input
+                      type="radio"
+                      name="membershipTier"
+                      value="basic"
+                      checked={membershipTier === "basic"}
+                      onChange={() => setMembershipTier("basic")}
+                      className="mr-2 text"
+                    />
+                    Basic
+                  </label>
+                  <label className="flex text items-center">
+                    <input
+                      type="radio"
+                      name="membershipTier"
+                      value="verified"
+                      checked={membershipTier === "verified"}
+                      onChange={() => setMembershipTier("verified")}
+                      className="mr-2 text"
+                    />
+                    <Image src="/icons/verified2.png" alt="Verified Icon" width={20} height={20} />
+                    Verified
+                  </label>
+                </div>
+                <label className="block mt-4 text">Profile Type</label>
                 <select
                   className="w-full p-2 border rounded-md bg-transparent"
                   value={profile}
@@ -173,7 +160,7 @@ export default function AuthForm({ isDarkMode }: { isDarkMode: boolean }) {
                     </option>
                   ))}
                 </select>
-                <label className="block mt-4">Role</label>
+                <label className="block mt-4 text">Role</label>
                 <select
                   className="w-full p-2 border rounded-md bg-transparent"
                   value={role}
@@ -194,8 +181,13 @@ export default function AuthForm({ isDarkMode }: { isDarkMode: boolean }) {
                     </option>
                   ))}
                 </select>
-                <label className="block mt-4">About/Introduction</label>
-                <textarea placeholder="Tell us about yourself or your project..." className="w-full p-2 border rounded-md bg-transparent" />
+                <label className="block mt-4 text">About/Introduction</label>
+                <textarea
+                  placeholder="Tell us about yourself or your project..."
+                  className="w-full p-2 border rounded-md bg-transparent"
+                  value={about}
+                  onChange={handleChange(setAbout)}
+                />
               </div>
             )}
           </div>
@@ -203,55 +195,73 @@ export default function AuthForm({ isDarkMode }: { isDarkMode: boolean }) {
 
         {/* Right Column: Dynamic Preview */}
         {(currentStep === 2 || currentStep === 3) && (
-          <div className="w-full md:w-1/2 h-auto p-4 md:sticky md:top-[10vh] flex flex-col items-center justify-center">
+          <div className="w-full md:w-1/2 p-4 md:sticky self-center flex flex-col items-center">
             <div className={`${containerStyle} w-full`}>
-              <h3 className="font-bold mb-4">Preview</h3>
-              <div className="leading-relaxed text-gray-300">
-                <div className="flex flex-col items-center space-y-4">
-                  {/* Profile Picture Preview */}
-                  <div className="w-24 h-24 rounded-full overflow-hidden border border-gray-300">
-                    {profilePicture ? (
-                      <Image src={profilePicture} alt="Profile Preview" width={96} height={96} />
-                    ) : (
-                      <div className="flex items-center justify-center w-full h-full bg-gray-200">
-                        <span>Placeholder</span>
-                      </div>
-                    )}
+              <div className="relative w-full mb-4 h-32 bg-gray-700 rounded-md flex justify-center items-center">
+                {bannerImage ? (
+                  <Image
+                    src={bannerImage}
+                    alt="Banner Preview"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-md"
+                  />
+                ) : (
+                  <span className="text-gray-400">Banner Placeholder</span>
+                )}
+              </div>
+              <label className="custom-upload-button" htmlFor="banner-upload">
+                <input
+                  type="file"
+                  id="banner-upload"
+                  className="hidden"
+                  onChange={(e) => handleImageUpload(e, setBannerImage)}
+                />
+                <div className={customUploadButtonStyle}>Upload Banner Image</div>
+              </label>
+              <div className="relative w-24 h-24 mt-4 mb-2 rounded-full overflow-hidden border border-gray-300">
+                {profilePicture ? (
+                  <Image
+                    src={profilePicture}
+                    alt="Profile Preview"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                ) : (
+                  <div className="flex items-center justify-center w-full h-full bg-gray-200">
+                    <span>Profile Placeholder</span>
                   </div>
-                  {/* Display Name */}
-                  <div>
-                    <h3 className="font-bold">Display Name:</h3>
-                    <p>{displayName || "Your display name will appear here."}</p>
-                  </div>
-                  {/* Username */}
-                  <div>
-                    <h3 className="font-bold">Username:</h3>
-                    <p>@{username || "username"}</p>
-                  </div>
-                  {/* About */}
-                  <div>
-                    <h3 className="font-bold">About:</h3>
-                    <p>{about || "Your introduction will appear here."}</p>
-                  </div>
-                  {/* Upload Profile Picture for larger screens */}
-                  {currentStep === 2 && (
-                    <div className="hidden md:block mt-4">
-                      <label className="block">Upload Profile Picture</label>
-                      <input type="file" className="w-full" onChange={handleImageUpload} />
-                    </div>
+                )}
+              </div>
+              <label className="custom-upload-button" htmlFor="profile-upload">
+                <input
+                  type="file"
+                  id="profile-upload"
+                  className="hidden"
+                  onChange={(e) => handleImageUpload(e, setProfilePicture)}
+                />
+                <div className={customUploadButtonStyle}>Upload Profile Image</div>
+              </label>
+              <div className="mt-4 text-left w-full">
+                <h3 className="font-bold text-lg flex flex-row">
+                  {displayName || "Display Name"}{" "}
+                  {membershipTier === "verified" && (
+                    <Image src="/icons/verified2.png" alt="Verified" width={16} height={16} />
                   )}
-                </div>
-                {currentStep === 3 && renderMembershipComparison()}
+                </h3>
+                <p className="text-sm text-gray-400">@{username || "username"}</p>
+                <p className="mt-2 text">{about || "Your introduction will appear here."}</p>
               </div>
             </div>
           </div>
         )}
       </div>
 
-      {/* Fixed Navigation Buttons */}
-      <div className="fixed bottom-4 right-4 md:bottom-8 md:right-8 flex space-x-4">
+      {/* Navigation Buttons */}
+      <div className="mt-4 md:fixed md:bottom-4 md:right-4 flex justify-center md:justify-end space-x-4">
         {currentStep > 1 && (
-          <button onClick={prevStep} className="p-2 border rounded-md hover:bg-gray-100 hover:text-yellow-700 transition">
+          <button onClick={prevStep} className="p-2 text border rounded-md hover:bg-gray-100 hover:text-yellow-700 transition">
             Back
           </button>
         )}
