d3e37406945b9771353334f1158e97a2d4e2e6d8
sean00880 <shantanu1997@gmail.com>
2024-11-23 07:17:39 -0800

    Edit45

--- src/app/api/[username]/page.tsx
+++ src/app/api/[username]/page.tsx
@@ -1,7 +1,11 @@
-import { getProfileImageUrl } from '../../../utils/imageUtils';
-import Image from 'next/image';
+import { getProfileImageUrl } from "../../../utils/imageUtils";
+import Image from "next/image";
 
-export default function UserProfile({ params }: { params: { username: string } }) {
+interface UserProfileProps {
+  params: { username: string };
+}
+
+export default function UserProfile({ params }: UserProfileProps) {
   const { username } = params;
 
   return (
@@ -9,14 +13,14 @@ export default function UserProfile({ params }: { params: { username: string } }
       <h1>{username}&apos;s Profile</h1>
       <div className="images">
         <Image
-          src={getProfileImageUrl(username, 'profile')}
+          src={getProfileImageUrl(username, "profile")}
           alt={`${username}'s Profile`}
           className="profile-image"
           width={600}
           height={200}
         />
         <Image
-          src={getProfileImageUrl(username, 'banner')}
+          src={getProfileImageUrl(username, "banner")}
           alt={`${username}'s Banner`}
           className="banner-image"
           height={150}
